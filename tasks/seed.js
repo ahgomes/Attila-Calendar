1aadd7c233fc4d36083567518a646bc605dccf1e
jruan2 <jruan2@stevens.edu>
2022-05-08 00:23:24 -0400

    Made seed file more random

--- tasks/seed.js
+++ tasks/seed.js
@@ -1,62 +1,59 @@
-/* Here are all the dummy user accounts. Users 3-5 might be of interest because they have randomly generated event data
- * |----------|----------|
- * | USERNAME | PASSWORD |
- * |----------|----------|
- * | user1    | abc123   |
- * |----------|----------|
- * | user2    | abc123   |
- * |----------|----------|
- * | user3    | abc123   |
- * |----------|----------|
- * | user4    | abc123   |
- * |----------|----------|
- * | user5    | abc123   |
- * |----------|----------|
+/* Here are all the dummy user accounts. Users 3-5 might be of interest because they have randomly generated event data.
+ * thanks to https://github.com/marcoagpinto/aoo-mozilla-en-dict for the word dictionary list
+ *
+ * |----------|----------|--------------------------------------------------------------------|
+ * | USERNAME | PASSWORD | NOTES                                                              |
+ * |----------|----------|--------------------------------------------------------------------|
+ * | venkat   | abc123   | Contains 5 events with unique titles, descriptions, and priorities |
+ * |----------|----------|--------------------------------------------------------------------|
+ * | antonio  | abc123   | Contains 1 event with 5 unique user comments                       |
+ * |----------|----------|--------------------------------------------------------------------|
+ * | adrian   | abc123   | Contains randomly generated event and comment data                 |
+ * |----------|----------|--------------------------------------------------------------------|
+ * | jason    | abc123   | Contains randomly generated event and comment data                 |
+ * |----------|----------|--------------------------------------------------------------------|
+ * | jacob    | abc123   | Contains randomly generated event and comment data                 |
+ * |----------|----------|--------------------------------------------------------------------|
  */
 
 const mongoConnection = require('../config/mongoConnection');
+const fileSys = require('fs');
 
 const data = require('../data');
 const usersApi = data.usersApi;
 const eventsApi = data.eventsApi;
+
+let words = null;
+fileSys.readFile('./tasks/wordlist.txt', 'utf-8', (err, data) => {
+    words = data.split('\n');
+});
 
 function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function randomWords(numWords) {
-    let result = '';
-    const letters =
-        'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    result = '';
     for (let i = 0; i < numWords; i++) {
-        let word = '';
-        for (let j = 0; j <= randomInt(3, 8); j++) {
-            word += letters[randomInt(0, letters.length - 1)];
-        }
-        result += i === 0 ? word : ` ${word}`;
+        result += `${words[randomInt(0, words.length - 1)]} `;
     }
-    return result;
+    return result.substring(0, result.length - 1);
 }
 
 async function main() {
     const db = await mongoConnection.connectToDb();
     await db.dropDatabase();
 
-    let user1 = null,
-        user2 = null,
-        user3 = null,
-        user4 = null,
-        user5 = null;
-
-    let calendarId = null,
+    let user = null,
+        calendarId = null,
         eventId = null;
 
     // create user1
     try {
         console.log('Creating user1...');
-        await usersApi.createUser('user1', 'abc123', 'John', 'Smith');
-        user1 = await usersApi.getLoggedinUser({ session: { user: 'user1' } });
-        calendarId = user1.calendars[0]._id.toString();
+        await usersApi.createUser('venkat', 'abc123', 'Venkat', 'Anna');
+        user = await usersApi.getLoggedinUser({ session: { user: 'venkat' } });
+        calendarId = user.calendars[0]._id.toString();
         console.log('Successfully created user1');
     } catch (e) {
         console.log('Failed to create user1:');
@@ -65,11 +62,11 @@ async function main() {
 
     // create events for user1
     try {
-        if (user1 && calendarId) {
+        if (user && calendarId) {
             console.log('Creating events for user1...');
             for (let i = 1; i <= 5; i++) {
                 await eventsApi.createNewEvent(
-                    user1.username,
+                    user.username,
                     calendarId,
                     `Title ${i}`,
                     `Description ${i}`,
@@ -77,6 +74,7 @@ async function main() {
                     new Date()
                 );
             }
+            user = null;
             calendarId = null;
             console.log('Successfully created events for user1');
         }
@@ -90,9 +88,11 @@ async function main() {
     // create user2
     try {
         console.log('Creating user2...');
-        await usersApi.createUser('user2', 'abc123', 'Adam', 'Miller');
-        user2 = await usersApi.getLoggedinUser({ session: { user: 'user2' } });
-        calendarId = user2.calendars[0]._id.toString();
+        await usersApi.createUser('antonio', 'abc123', 'Antonio', 'Cardona');
+        user = await usersApi.getLoggedinUser({
+            session: { user: 'antonio' },
+        });
+        calendarId = user.calendars[0]._id.toString();
         console.log('Successfully created user2');
     } catch (e) {
         console.log('Failed to create user2:');
@@ -101,11 +101,11 @@ async function main() {
 
     // create events for user2
     try {
-        if (user2 && calendarId) {
+        if (user && calendarId) {
             console.log('Creating events for user2...');
             eventId = (
                 await eventsApi.createNewEvent(
-                    user2.username,
+                    user.username,
                     calendarId,
                     'CS-546 Final Project',
                     'Must complete the project code and presentation by May 8, 11:59 PM',
@@ -116,11 +116,12 @@ async function main() {
             for (let i = 4; i >= 0; i--) {
                 await eventsApi.addCommentById(
                     eventId,
-                    user2.username,
+                    user.username,
                     `Uh oh ${5 * i} minutes until the deadline!!!`,
                     new Date(2022, 4, 8, 23, 59 - 5 * i)
                 );
             }
+            user = null;
             calendarId = null;
             eventId = null;
             console.log('Successfully created events for user2');
@@ -132,179 +133,82 @@ async function main() {
 
     console.log();
 
-    // create user3
-    try {
-        console.log('Creating user3...');
-        await usersApi.createUser('user3', 'abc123', 'Mary', 'Lopez');
-        user3 = await usersApi.getLoggedinUser({ session: { user: 'user3' } });
-        calendarId = user3.calendars[0]._id.toString();
-        console.log('Successfully created user3');
-    } catch (e) {
-        console.log('Failed to create user3:');
-        console.log(e);
-    }
-
-    // create events for user3
-    try {
-        if (user3 && calendarId) {
-            console.log('Creating events for user3...');
-            for (let i = 0; i < randomInt(8, 10); i++) {
-                eventId = (
-                    await eventsApi.createNewEvent(
-                        user3.username,
-                        calendarId,
-                        randomWords(20),
-                        randomWords(100),
-                        randomInt(1, 5),
-                        new Date(
-                            randomInt(2000, 2030),
-                            randomInt(0, 11),
-                            randomInt(0, 25),
-                            randomInt(0, 23),
-                            randomInt(0, 59)
-                        )
-                    )
-                )._id.toString();
-            }
-            for (let i = 0; i < randomInt(0, 5); i++) {
-                await eventsApi.addCommentById(
-                    eventId,
-                    user3.username,
-                    randomWords(20),
-                    new Date(
-                        randomInt(2000, 2030),
-                        randomInt(0, 11),
-                        randomInt(0, 25),
-                        randomInt(0, 23),
-                        randomInt(0, 59)
-                    )
+    if (words) {
+        const users = [
+            ['user3', 'adrian', 'abc123', 'Adrian', 'Gomes'],
+            ['user4', 'jason', 'abc123', 'Jason', 'Ruan'],
+            ['user5', 'jacob', 'abc123', 'Jacob', 'Wood'],
+        ];
+        for (let i = 0; i < users.length; i++) {
+            // create users 3-5
+            try {
+                console.log(`Creating ${users[i][0]}...`);
+                await usersApi.createUser(
+                    users[i][1],
+                    users[i][2],
+                    users[i][3],
+                    users[i][4]
                 );
+                user = await usersApi.getLoggedinUser({
+                    session: { user: users[i][1] },
+                });
+                calendarId = user.calendars[0]._id.toString();
+                console.log(`Successfully created ${users[i][0]}`);
+            } catch (e) {
+                console.log(`Failed to create ${users[i][0]}:`);
+                console.log(e);
             }
-            calendarId = null;
-            eventId = null;
-            console.log('Successfully created events for user3');
+
+            // create events for users 3-5
+            try {
+                if (user && calendarId) {
+                    console.log(`Creating events for ${users[i][0]}...`);
+                    for (let j = 0; j < randomInt(8, 10); j++) {
+                        eventId = (
+                            await eventsApi.createNewEvent(
+                                user.username,
+                                calendarId,
+                                randomWords(20),
+                                randomWords(100),
+                                randomInt(1, 5),
+                                new Date(
+                                    randomInt(2000, 2030),
+                                    randomInt(0, 11),
+                                    randomInt(0, 25),
+                                    randomInt(0, 23),
+                                    randomInt(0, 59)
+                                )
+                            )
+                        )._id.toString();
+                        for (let k = 0; k < randomInt(0, 5); k++) {
+                            await eventsApi.addCommentById(
+                                eventId,
+                                user.username,
+                                randomWords(20),
+                                new Date(
+                                    randomInt(2000, 2030),
+                                    randomInt(0, 11),
+                                    randomInt(0, 25),
+                                    randomInt(0, 23),
+                                    randomInt(0, 59)
+                                )
+                            );
+                        }
+                    }
+                    user = null;
+                    calendarId = null;
+                    eventId = null;
+                    console.log(
+                        `Successfully created events for ${users[i][0]}`
+                    );
+                }
+            } catch (e) {
+                console.log(`Failed to create events ${users[i][0]}:`);
+                console.log(e);
+            }
+
+            console.log();
         }
-    } catch (e) {
-        console.log('Failed to create events user3:');
-        console.log(e);
-    }
-
-    console.log();
-
-    // create user4
-    try {
-        console.log('Creating user4...');
-        await usersApi.createUser('user4', 'abc123', 'Thomas', 'Anderson');
-        user4 = await usersApi.getLoggedinUser({ session: { user: 'user4' } });
-        calendarId = user4.calendars[0]._id.toString();
-        console.log('Successfully created user4');
-    } catch (e) {
-        console.log('Failed to create user4:');
-        console.log(e);
-    }
-
-    // create events for user4
-    try {
-        if (user4 && calendarId) {
-            console.log('Creating events for user4...');
-            for (let i = 0; i < randomInt(8, 10); i++) {
-                eventId = (
-                    await eventsApi.createNewEvent(
-                        user4.username,
-                        calendarId,
-                        randomWords(20),
-                        randomWords(100),
-                        randomInt(1, 5),
-                        new Date(
-                            randomInt(2000, 2030),
-                            randomInt(0, 11),
-                            randomInt(0, 25),
-                            randomInt(0, 23),
-                            randomInt(0, 59)
-                        )
-                    )
-                )._id.toString();
-            }
-            for (let i = 0; i < randomInt(0, 5); i++) {
-                await eventsApi.addCommentById(
-                    eventId,
-                    user4.username,
-                    randomWords(20),
-                    new Date(
-                        randomInt(2000, 2030),
-                        randomInt(0, 11),
-                        randomInt(0, 25),
-                        randomInt(0, 23),
-                        randomInt(0, 59)
-                    )
-                );
-            }
-            calendarId = null;
-            eventId = null;
-            console.log('Successfully created events for user4');
-        }
-    } catch (e) {
-        console.log('Failed to create events user4:');
-        console.log(e);
-    }
-
-    console.log();
-
-    // create user5
-    try {
-        console.log('Creating user5...');
-        await usersApi.createUser('user5', 'abc123', 'Harris', 'Perez');
-        user5 = await usersApi.getLoggedinUser({ session: { user: 'user5' } });
-        calendarId = user5.calendars[0]._id.toString();
-        console.log('Successfully created user5');
-    } catch (e) {
-        console.log('Failed to create user5:');
-        console.log(e);
-    }
-
-    // create events for user4
-    try {
-        if (user5 && calendarId) {
-            console.log('Creating events for user5...');
-            for (let i = 0; i < randomInt(8, 10); i++) {
-                eventId = (
-                    await eventsApi.createNewEvent(
-                        user5.username,
-                        calendarId,
-                        randomWords(20),
-                        randomWords(100),
-                        randomInt(1, 5),
-                        new Date(
-                            randomInt(2000, 2030),
-                            randomInt(0, 11),
-                            randomInt(0, 25),
-                            randomInt(0, 23),
-                            randomInt(0, 59)
-                        )
-                    )
-                )._id.toString();
-            }
-            for (let i = 0; i < randomInt(0, 5); i++) {
-                await eventsApi.addCommentById(
-                    eventId,
-                    user5.username,
-                    randomWords(20),
-                    new Date(
-                        randomInt(2000, 2030),
-                        randomInt(0, 11),
-                        randomInt(0, 25),
-                        randomInt(0, 23),
-                        randomInt(0, 59)
-                    )
-                );
-            }
-            calendarId = null;
-            eventId = null;
-            console.log('Successfully created events for user5');
-        }
-    } catch (e) {
-        console.log('Failed to create events user5:');
-        console.log(e);
     }
 
     console.log('Done');
